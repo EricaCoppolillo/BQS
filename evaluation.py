4a89d017e62457fc26956abe68a5d1aba6166ecb
francesco <samjack84@gmail.com>
2020-09-22 17:35:43 +0200

    add old implementation of hitrate
    add old implementation of hitrate

--- evaluation.py
+++ evaluation.py
@@ -150,6 +150,115 @@ class MetricAccumulator:
             accumulator.recall_by_pop += np.array(hit_pop)
 
 
+class OldMetricAccumulator:
+    def __init__(self):
+        self.data = collections.defaultdict(SimpleMetric)
+
+    def reset(self):
+        self.data.clear()
+
+    def get_top_k(self):
+        return sorted(self.data.keys())
+
+    def get_metrics(self, top_k=None):
+        result = {}
+
+        with np.errstate(divide='ignore', invalid='ignore'):
+            k_list = [top_k] if top_k else self.data.keys()
+            for k in k_list:
+                acc = self.data[k]
+
+                computed_acc = SimpleMetric()
+                computed_acc.recall = acc.recall / acc._total_positives.sum()
+                computed_acc.recall_by_pop = acc.recall_by_pop / acc._total_positives
+                computed_acc.recall_by_pop[np.isnan(computed_acc.recall_by_pop)] = 0
+
+                result[k] = computed_acc
+
+        return result[top_k] if top_k else result
+
+    def compute_metric(self, x, y, pos, neg, popularity, popularity_thresholds, top_k=10):
+        """
+            Compute metric:
+            - hitrate
+            - hitrate per popularity
+            - weighted hit rate
+
+            :param x: user preferences, BS x items
+            :param y: user predictions, BS x items
+            :param mask: user selected preferences, BS x 100
+            :param popularity: dict of item popularity (normalized)
+            :param top_k: top k items to select ranked by score
+            :return: hitrate, popularity hitrate, total_positives(low,medium,high), weighted_hr
+            """
+        assert min([len(r) for r in
+                    neg]) >= top_k and top_k > 0, f"fail with top_k = {top_k} and neg = {[len(r) for r in neg]}"
+        assert len(popularity) == y.shape[-1], f'{len(popularity)} != {y.shape[-1]}'
+        avg_hr = 0
+        weighted_hr = 0
+        total_positives = np.zeros(3)
+        avg_hits = np.zeros(3)
+
+        for i in range(y.shape[0]):
+            input_idx = np.where(x[i, :] == 1)[0]
+            score = y[i, :]
+
+            viewed_item = set(input_idx)
+            # print('LC > viewed_item:',viewed_item)
+            positive_items = set(pos[i])
+            negative_items = neg[i]
+            neg_scores = sorted(score[negative_items].tolist(), reverse=True)
+
+            # Tutti i positivi predetti meno quelli visti
+            predicted_item = positive_items - viewed_item
+            hit = 0
+            weight_hit = 0
+            hit_pop = [0, 0, 0]
+            hit_pop_tot = [0, 0, 0]
+
+            for pos_item in predicted_item:
+                score_pos = score[pos_item]
+                current_popularity = popularity[pos_item]
+                score_top_k = neg_scores[top_k - 1]
+
+                if score_pos > score_top_k:
+                    hit += 1
+                    weight_hit += 1 - current_popularity
+
+                # popularity
+                if current_popularity <= popularity_thresholds[0]:
+
+                    hit_pop_tot[0] += 1
+                    if score_pos > score_top_k:
+                        hit_pop[0] += 1
+
+                elif popularity_thresholds[0] < current_popularity <= popularity_thresholds[1]:
+
+                    hit_pop_tot[1] += 1
+                    if score_pos > score_top_k:
+                        hit_pop[1] += 1
+
+                else:  # current_popularity > popularity_thresholds[1]
+
+                    hit_pop_tot[2] += 1
+                    if score_pos > score_top_k:
+                        hit_pop[2] += 1
+
+            assert hit <= len(predicted_item), f'{hit} / {len(predicted_item)}'
+            assert sum(hit_pop) == hit, f'hit count error {hit} != {hit_pop}'
+            assert len(predicted_item) == sum(hit_pop_tot), f'hit count error {len(predicted_item)} != {hit_pop_tot}'
+
+            # avg_hr += hit
+            # avg_hits += np.array(hit_pop)
+            # total_positives += np.array(hit_pop_tot)
+
+            accumulator = self.data[top_k]
+            accumulator._total_positives += np.array(hit_pop_tot)
+            accumulator._num_users += 1
+            accumulator.recall += hit
+            accumulator.recall_by_pop += np.array(hit_pop)
+
+
 if __name__ == '__main__':
     acc = MetricAccumulator()
 
