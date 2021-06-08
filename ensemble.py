5d9efe7ee12868496316f4634d91e0d071bfb322
mminici <acermarco95@gmail.com>
2021-06-08 09:29:44 +0200

    added ensemble config plus minor changes to ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -1,7 +1,5 @@
 import collections
 import os
-import random
-import types
 from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
@@ -10,36 +8,24 @@ import numpy as np
 import json
 
 from evaluation import MetricAccumulator
-from util import compute_max_y_aux_popularity, naive_sparse2tensor
+from util import compute_max_y_aux_popularity, naive_sparse2tensor, set_seed
 from data_loaders import EnsembleDataLoader
 from models import EnsembleMultiVAE
 from loss_func import ensemble_rvae_rank_pair_loss
-
-# DATASETS ----------------------------------------
-CITEULIKE = 'citeulike-a'
-EPINIONS = 'epinions'
-MOVIELENS_1M = 'ml-1m'
-MOVIELENS_20M = 'ml-20m'
-NETFLIX = 'netflix'
-NETFLIX_SAMPLE = 'netflix_sample'
-PINTEREST = 'pinterest'
-# ---------------------------------------------------
+from constants import datasets
+from config import Config
 
 # SETTINGS ------------------------------------------
-dataset_name = MOVIELENS_1M
+dataset_name = datasets.MOVIELENS_1M
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+config = Config("./rvae_config.json")
+config.metrics_scale = eval(config.metrics_scale)
+config.use_popularity = eval(config.use_popularity)
+config.p_dims = eval(config.p_dims)
 # ---------------------------------------------------
 
 SEED = 8734
-
-np.random.seed(SEED)
-random.seed(SEED)
-torch.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-
-if 'CUDA_VISIBLE_DEVICES' not in os.environ:
-    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+set_seed(SEED)
 
 USE_CUDA = True
 CUDA = USE_CUDA and torch.cuda.is_available()
@@ -83,51 +69,15 @@ In train the data is reported with a mask of *x* items selected randomly between
 In test the data is reported with 3 masks of items with less, middle and top popolarity
 """
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Dataloader
 trainloader = EnsembleDataLoader(data_dir, use_popularity=True)
 n_items = trainloader.n_items
+config.p_dims.append(n_items)
 thresholds = trainloader.thresholds
 popularity = trainloader.item_popularity
 frequencies = trainloader.frequencies
 
-# SETTINGS
-settings = types.SimpleNamespace()
-settings.dataset_name = os.path.split(data_dir)[-1]
-settings.p_dims = [200, 600, n_items]
-# settings.p_dims = [1, 5, 10, 50, 100, 200, 600, n_items]
-settings.batch_size = 1024
-settings.weight_decay = 0.0
-settings.learning_rate = 1e-3
-# the total number of gradient updates for annealing
-settings.total_anneal_steps = 200000
-# largest annealing parameter
-settings.anneal_cap = 0.2
-settings.sample_mask = 100
-settings.gamma_k = 1000
-settings.metrics_alpha = 100
-settings.metrics_beta = .03
-settings.metrics_gamma = 5
-settings.metrics_scale = 1 / 15
-settings.metrics_percentile = .45
-max_y_aux_popularity = compute_max_y_aux_popularity(settings)
-
-
-
-
-
+max_y_aux_popularity = compute_max_y_aux_popularity(config)
 top_k = (1, 5, 10)
 
 
@@ -143,7 +93,7 @@ def evaluate(dataloader, tag='validation'):
 
     with torch.no_grad():
         for batch_idx, (x, pos, neg, mask, pos_te, neg_te, mask_te, y_a, y_b) in enumerate(
-                dataloader.iter_test_ensemble(batch_size=settings.batch_size, tag=tag, device=device)):
+                dataloader.iter_test_ensemble(batch_size=config.batch_size, tag=tag, device=device)):
             x_tensor = naive_sparse2tensor(x).to(device)
             pos = naive_sparse2tensor(pos).to(device)
             neg = naive_sparse2tensor(neg).to(device)
@@ -182,19 +132,12 @@ def evaluate(dataloader, tag='validation'):
 torch.set_printoptions(profile="full")
 n_epochs = 1
 update_count = 0
-settings.batch_size = 1024  # 256
-# settings.learning_rate = 1e-4  # 1e-5
-settings.learning_rate = 1e-2  # 1e-5
-settings.optim = 'adam'
-settings.scale = 1000
-settings.use_popularity = True
-settings.p_dims = [200, 600, n_items]
 
 model = EnsembleMultiVAE(n_items, popularity, thresholds)
 model = model.to(device)
 
 criterion = ensemble_rvae_rank_pair_loss(popularity=popularity,
-                                scale=settings.scale,
+                                scale=config.scale,
                                 thresholds=thresholds,
                                 frequencies=frequencies)
 best_loss = np.Inf
@@ -214,15 +157,14 @@ lossname = criterion.__class__.__name__
 
 # result info
 with open(os.path.join(run_dir, 'info.txt'), 'w') as fp:
-    fp.write(f'K = {settings.gamma_k}\n')
+    fp.write(f'K = {config.gamma_k}\n')
     fp.write(f'Loss = {lossname}\n')
     fp.write(f'Epochs train = {len(stat_metric)}\n')
 
     fp.write('\n')
 
-    for k in dir(settings):
-        if not k.startswith('__'):
-            fp.write(f'{k} = {getattr(settings, k)}\n')
+    for k in config.__dict__:
+        fp.write(f'{k} = {config.__dict__[k]}\n')
 
 # all results
 with open(os.path.join(run_dir, 'result.json'), 'w') as fp:
