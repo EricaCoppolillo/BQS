6cbad9f96f4830bae0646eee9e89007dce6c5cc2
mminici <acermarco95@gmail.com>
2021-06-12 11:41:35 +0200

    added script to execute only the rvae baseline + minor changes to default hyper-params (reduced lr)

---
+++ exec/rvae_baseline_pipeline.sh
@@ -0,0 +1,13 @@
+# pick dataset(s) of interest - MOVIELENS_1M MOVIELENS_20M CITEULIKE PINTEREST EPINIONS NETFLIX
+declare -a datasets=("MOVIELENS_20M" "NETFLIX" "EPINIONS")
+declare -A datasets_path_name=( ["MOVIELENS_1M"]="ml-1m" ["MOVIELENS_20M"]="ml-20m" ["CITEULIKE"]="citeulike-a" ["PINTEREST"]="pinterest" ["EPINIONS"]="epinions" ["NETFLIX"]="netflix")
+# moving in the parent directory
+cd ../
+
+# iterating across datasets
+for dataset_name in "${datasets[@]}"; do
+  # loading default config file - TODO: Add capability to use custom files
+  cat default_rvae_configs.json | python -c "import sys, json; from util import clean_json_string; x=json.load(sys.stdin); x=x[sys.argv[1]]; x['baseline_flag']=str("True"); print(clean_json_string(str(x)))" $dataset_name > rvae_config.json
+  # running the baseline model training
+  python rvae.py
+done
