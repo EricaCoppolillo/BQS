2f94237de3a2fc8874b173b0f32282a0e35121b6
mminici <acermarco95@gmail.com>
2021-07-03 12:56:21 +0200

    settings for ensemble pipeline + new script to grid search optimal ensemble_weight

--- exec/rvae_ensemble_pipeline.sh
+++ exec/rvae_ensemble_pipeline.sh
@@ -1,15 +1,15 @@
 # pick dataset(s) of interest - MOVIELENS_1M MOVIELENS_20M CITEULIKE PINTEREST EPINIONS NETFLIX
-declare -a datasets=("MOVIELENS_20M" "NETFLIX")
+declare -a datasets=("MOVIELENS_1M" "CITEULIKE" "PINTEREST" "EPINIONS")
 declare -A datasets_path_name=( ["MOVIELENS_1M"]="ml-1m" ["MOVIELENS_20M"]="ml-20m" ["CITEULIKE"]="citeulike-a" ["PINTEREST"]="pinterest" ["EPINIONS"]="epinions" ["NETFLIX"]="netflix")
-cuda="2"
+cuda="1"
 # moving in the parent directory
 cd ../
 
 # iterating across datasets
 for dataset_name in "${datasets[@]}"; do
   # loading default config file - TODO: Add capability to use custom files
-  cat default_rvae_configs.json | python -c "import sys, json; from util import clean_json_string; x=json.load(sys.stdin); x=x[sys.argv[1]]; x['CUDA_VISIBLE_DEVICES']=sys.argv[2]; print(clean_json_string(str(x)))" $dataset_name $cuda > rvae_config.json
+  cat default_ensemble_configs.json | python -c "import sys, json; from util import clean_json_string; x=json.load(sys.stdin); x=x[sys.argv[1]]; x['CUDA_VISIBLE_DEVICES']=sys.argv[2]; print(clean_json_string(str(x)))" $dataset_name $cuda > ensemble_config.json
   echo "Currently analyzing: "$dataset_name
   # running the baseline model training
-  python rvae.py
+  python ensemble.py
 done
