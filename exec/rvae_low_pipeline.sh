2205948657db1934723103bfec00eec5b42dc16f
mminici <acermarco95@gmail.com>
2021-07-05 11:53:44 +0200

    added README to explain rvae_config.json parameters + minor changes to executables

--- exec/rvae_low_pipeline.sh
+++ exec/rvae_low_pipeline.sh
@@ -1,5 +1,5 @@
 # pick dataset(s) of interest - MOVIELENS_1M MOVIELENS_20M CITEULIKE PINTEREST EPINIONS NETFLIX
-declare -a datasets=("MOVIELENS_20M" "NETFLIX")
+declare -a datasets=("MOVIELENS_1M" "CITEULIKE" "PINTEREST" "MOVIELENS_20M" "NETFLIX" "EPINIONS")
 declare -A datasets_path_name=( ["MOVIELENS_1M"]="ml-1m" ["MOVIELENS_20M"]="ml-20m" ["CITEULIKE"]="citeulike-a" ["PINTEREST"]="pinterest" ["EPINIONS"]="epinions" ["NETFLIX"]="netflix")
 cuda="2"
 # moving in the parent directory
@@ -10,6 +10,6 @@ for dataset_name in "${datasets[@]}"; do
   # loading default config file - TODO: Add capability to use custom files
   cat default_rvae_configs.json | python -c "import sys, json; from util import clean_json_string; x=json.load(sys.stdin); x=x[sys.argv[1]]; x['CUDA_VISIBLE_DEVICES']=sys.argv[2]; print(clean_json_string(str(x)))" $dataset_name $cuda > rvae_config.json
   echo "Currently analyzing: "$dataset_name
-  # running the baseline model training
+  # running the LOW model training
   python rvae.py
 done
