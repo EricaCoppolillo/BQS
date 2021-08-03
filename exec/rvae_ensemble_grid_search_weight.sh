9356ac1f26b7613385caa918569f6a1eaac08167
francesco <samjack84@gmail.com>
2021-08-03 10:45:38 +0200

    update ensemble configs
    update ensemble configs

--- exec/rvae_ensemble_grid_search_weight.sh
+++ exec/rvae_ensemble_grid_search_weight.sh
@@ -1,13 +1,29 @@
 # pick dataset(s) of interest - MOVIELENS_1M MOVIELENS_20M CITEULIKE PINTEREST EPINIONS NETFLIX
-declare -a datasets=("MOVIELENS_20M")
-declare -A datasets_path_name=( ["MOVIELENS_1M"]="ml-1m" ["MOVIELENS_20M"]="ml-20m" ["CITEULIKE"]="citeulike-a" ["PINTEREST"]="pinterest" ["EPINIONS"]="epinions" ["NETFLIX"]="netflix")
-cuda="2"
+cuda="0"
 # moving in the parent directory
-cd ../
+cd $(dirname $0)/../
+
+DATASETS="CITEULIKE 0.2 0.05 0.7
+MOVIELENS_1M 0.2 0.05 0.7
+PINTEREST 0.5 0.05 0.8
+EPINIONS 0.3 0.05 0.45
+MOVIELENS_20M 2.0 0.1 3.0
+NETFLIX 0.25 0.05 0.4
+"
+
+DATASETS="NETFLIX 0.25 0.05 0.4
+"
 
 # iterating across datasets
-for dataset_name in "${datasets[@]}"; do
-  for i in $(seq 2.0 0.1 3.0); do
+#for dataset_name in "${datasets[@]}"; do
+
+echo -ne "$DATASETS" | while read line; do
+  dataset_name=$(echo -ne "$line" | awk '{print $1}')
+  params=$(echo -ne "$line" | awk '{print $2, $3, $4}')
+
+  echo "$dataset_name -- $params"
+  sleep 5s
+  for i in $(seq $params); do
     new_i="${i//,/.}"
     # loading default config file - TODO: Add capability to use custom files
     cat default_ensemble_configs.json | python -c "import sys, json; from util import clean_json_string; x=json.load(sys.stdin); x=x[sys.argv[1]]; x['CUDA_VISIBLE_DEVICES']=sys.argv[2]; x['ensemble_weight']=float(sys.argv[3]); print(clean_json_string(str(x)))" $dataset_name $cuda $new_i > ensemble_config.json
